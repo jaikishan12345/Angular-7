df55bb174a2e952a6c82d7ddb8b5b887e7eaf1e8
jaikishan <imjaikishan@gmail.com>
2019-01-15 12:10:02 +0530

    Implemented string interpolation in data binding

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessAlertComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
-
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 }
